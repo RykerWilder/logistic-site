cd5d6abc3d069f551c8f921d093fd9bd8e86a85e
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2025-03-12 22:25:01 +0100

    feat: AppFooter and FooterCard

---
+++ src/components/FooterCard.vue
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<template>
+    <div class="bg-slate-600 rounded-md p-5">
+        <h2>Titolo</h2>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+</template>
+
+<style scoped></style>
