4b86c1bb33594d5dc73cba716297c8854dc789e6
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2025-03-12 12:34:29 +0100

    formatting text

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import './style/general.scss';
-import {router} from './router';
+import { router } from './router';
 import App from './App.vue'
 
 createApp(App).use(router).mount('#app');
